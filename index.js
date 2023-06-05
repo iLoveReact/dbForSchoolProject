8327c6f256dceef3c22a14b4ec331f450781ea24
iLoveReact <maksym.slotwinski2006@gmail.com>
2023-06-05 12:39:31 +0200

    populate schedule

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import getHouses from "./getHouses.js";
 import createHouseAssociation from "./getHouseAssociation.js";
 const getAllHousingAssociationRes =  await getAllHousingAssociation();
 if (getAllHousingAssociationRes.error) console.error("an error has occured")
-else console.log("failed to get all Housing Associations");
 await getHouses(); // error handle by checking whether houses.csv exists
-await createHouseAssociation();
+// createHouseAssociation();
